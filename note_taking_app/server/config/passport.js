98c1cc6d46fe1248b219d38dee7fa15d69787dda
mark <markfrancisantonio@yahoo.com>
2025-08-02 17:43:12 -0700

    setup passport authentication

---
+++ note_taking_app/server/config/passport.js
@@ -0,0 +1,37 @@
+
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "../models/User.js";
+
+export default function configurePassport(passport){ 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username });
+            if (!user) {
+                return done(null,false, {message: "Incorrect username"});
+            }
+            const isValid = await user.isValidPassword(password);
+            if (!isValid) {
+                return done(null, false, { message: "Incorrect password"});
+            }
+            
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    })
+);
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, { username: user.username });
+    } catch (error) {
+        done(error);
+    }
+});
+}
--- note_taking_app/server/index.js
+++ note_taking_app/server/index.js
@@ -1,20 +1,33 @@
 // setup express api
 import express from "express";
 import dotenv from "dotenv"
+import session from "express-session";
+import passport from "passport";
+
 import { connectDB } from "./config/db.js";
+import configurePassport from "./config/passport.js";
 import { router as noteRoutes } from "./routes/noteRoutes.js"
+
 
 dotenv.config()
 connectDB();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(session({
+    secret: "yourSecretKey",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+configurePassport(passport);
+
 app.use("/api/notes", noteRoutes);
-
-
 
 //listen on port 3000
 app.listen(PORT, () =>{
