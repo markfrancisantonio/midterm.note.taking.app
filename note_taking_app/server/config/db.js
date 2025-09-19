06a117dd4764b0f2373d6f5491d5e1b72df3ed8d
mark <markfrancisantonio@yahoo.com>
2025-09-18 19:47:52 -0700

    connect db to mongodb atlas.  moved .env file in server

--- note_taking_app/server/config/db.js
+++ note_taking_app/server/config/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 // Connect to MongoDB using URI from environment or fallback to local DB
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/myDatabase"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
